0927451bb527ae420cde4aa55010cf8da459094a
leticia-brand <leticia.brandao.gs@gmail.com>
2021-12-01 21:03:47 -0300

    update

--- mysite/delphone/urls.py
+++ mysite/delphone/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
