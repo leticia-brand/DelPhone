c7afe50b768dec8a3df4a48b3ca29230f99cecc8
Darlan369 <darlanaraujo369@gmail.com>
2021-12-01 23:47:22 -0300

    nav

--- mysite/delphone/urls.py
+++ mysite/delphone/urls.py
@@ -5,7 +5,14 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cadastro/', views.cadastro, name='cadastro')
+    path('menu/', views.menu, name='menu'),
+    path('contatos/', views.contatos, name='contatos'),
+    path('faq/', views.faq, name='faq'),
+    path('login/', views.login, name='login'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('como_funcionamos/', views.como_funcionamos, name='como_funcionamos'),
+    path('sobre_nos/', views.sobre_nos, name='sobre_nos')
+
     
 ]
 
--- mysite/delphone/views.py
+++ mysite/delphone/views.py
@@ -8,5 +8,24 @@ from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 def index(request):
     return render(request, 'delphone/index.html')
 
+def menu(request):
+    return render(request, 'delphone/menu.html')
+
+def contatos(request):
+    return render(request, 'delphone/contatos.html')
+
+def faq(request):
+    return render(request, 'delphone/faq.html')
+    
+def login(request):
+    return render(request, 'delphone/login.html')
+
 def cadastro(request):
     return render(request, 'delphone/cadastro.html')
+
+def como_funcionamos(request):
+    return render(request, 'delphone/como_funcionamos.html')
+
+def sobre_nos(request):
+    return render(request, 'delphone/sobre_nos.html')
+
