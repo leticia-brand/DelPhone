ff28847904c54c3bbc2066fba91d807ce953ce0f
leticia-brand <leticia.brandao.gs@gmail.com>
2021-12-03 02:25:31 -0300

    update

--- mysite/delphone/models.py
+++ mysite/delphone/models.py
@@ -1,21 +1,26 @@
+from typing import Reversible
 from faker import Faker
 import random
+import pandas as pd
+from django.db import models
 
 fake = Faker()
 
-from django.db import models
+
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text='Digite o nome da empresa')
     email = models.EmailField(max_length=50, help_text='Digite o email')
     celular = models.CharField(max_length=11, help_text='Digite o celular (12)345678912')
     endereco = models.CharField(max_length=200, help_text='Digite o endereço (rua e nº)')
+    estado =  models.CharField(max_length=2 , help_text='Digite o estado')
+    cidade =  models.CharField(max_length=200, help_text='Digite a cidade')
     horario_func = models.TimeField(auto_now=False, auto_now_add=False, help_text='Digite o horário de funcionamento')
     setor = models.CharField(max_length=30, help_text='')
 
     def get_absolute_url(self):
         """Returns the url to access a particular instance of Empresa."""
-        return reverse('empresa-detail', args=[str(self.id)])
+        return Reversible('empresa-detail', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the Empresa object (in Admin site etc.)."""
@@ -40,7 +45,7 @@ class Empresa(models.Model):
         ordering = ['user']
 '''
 
-estados = pd.read_excel (r'cidades_estados.xlsx').values.tolist()
+estados = pd.read_excel (r'delphone/cidades_estados.xlsx').values.tolist()
 setores = ['Alimentação', 'Cozinha', 'Eletrodomésticos', 'Esportes', 'Farmácia', 'Material de construção', 'Mobília', 'Música', 'Papelaria', 'Vestuário']
 
 for i in range(200):
