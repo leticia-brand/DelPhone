c38614570ef7a67de503acd902b4c541a9df2c23
PedroGarcia6 <limagarciapedro770@gmail.com>
2021-12-03 19:43:32 -0400

    tá fluindo, gente

--- mysite/pedro/views.py
+++ mysite/pedro/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.urls import reverse
+from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
+
+def index(request):
+    return render(request, "pedro/analysis.html")
+
+def redireciona(request):
+    direcionamento = reverse('pedro')
+    return HttpResponseRedirect(direcionamento)
