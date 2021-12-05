91a8a8f5c7e1d6a313ee9e6134d0c5776ebf092a
leticia-brand <leticia.brandao.gs@gmail.com>
2021-12-04 22:10:31 -0300

    ipynb to html

--- mysite/leticia/views.py
+++ mysite/leticia/views.py
@@ -2,18 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
 from django.http.response import HttpResponseNotFound, HttpResponseRedirect
-from delphone.models import Empresa, Busca
-import pandas as pd
 
 # Create your views here.
 
-empresas = Empresa.objects.all().values()
-empresas_df = pd.DataFrame(empresas)
-empresas_dfh = empresas_df.head()
-
 def  analise (request):
-    contexto = {"empresas_dfh": empresas_dfh.to_html()}
-    return render(request, "leticia/analysis.html", contexto)
+    return render(request, "leticia/analise.html")
 
 def redireciona(request):
     direcionamento = reverse('leticia')
